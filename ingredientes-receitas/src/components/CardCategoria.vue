a401cc9ecf2ba027515de055ada398238592e509
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-17 11:04:23 -0300

    Add CardCategoria component

---
+++ ingredientes-receitas/src/components/CardCategoria.vue
@@ -0,0 +1,59 @@
+<script lang="ts">
+import type { PropType } from "vue";
+import type { Categoria } from "../interfaces/Categoria";
+
+export default {
+  props: {
+    categoria: {
+      type: Object as PropType<Categoria>,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  {{ categoria.nome }}
+  {{ categoria.ingredientes }}
+  {{ categoria.imagem }}
+</template>
+
+<style scoped>
+.categoria {
+  width: 19.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: var(--branco, #fff);
+  box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.categoria__cabecalho {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.categoria__imagem {
+  width: 3.5rem;
+}
+
+.categoria__nome {
+  text-align: center;
+  color: var(--verde-medio, #3d6d4a);
+  font-weight: 700;
+}
+
+.categoria__ingredientes {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+}
+</style>
