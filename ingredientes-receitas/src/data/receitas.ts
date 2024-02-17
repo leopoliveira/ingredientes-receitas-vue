59ebc12a8da3b1f9443863ed2461642875fd0d3f
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-17 10:21:00 -0300

    Refactor obterCategorias function to use typed response.

--- ingredientes-receitas/src/data/receitas.ts
+++ ingredientes-receitas/src/data/receitas.ts
@@ -1,11 +1,13 @@
 import type { Categoria } from "../interfaces/Categoria"
 
-export async function obterCategorias():Promise<Categoria[]> {
+export async function obterCategorias(): Promise<Categoria[]> {
   const url = "https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json";
 
   const response = await fetch(url);
 
-  return await response.json();
+  const categorias: Categoria[] = await response.json();
+
+  return categorias;
 }
 
 
