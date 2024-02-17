dd5e6247e9f573eef1444d6f78447c23a2868e5b
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-16 22:22:50 -0300

    Add type annotation for listaCategorias variable

--- ingredientes-receitas/src/components/SelecionarIngredientes.vue
+++ ingredientes-receitas/src/components/SelecionarIngredientes.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import type { Categoria } from '../interfaces/Categoria';
 import { obterCategorias } from '../data/receitas';
 
-const listaCategorias = obterCategorias();
+const listaCategorias:Categoria[] = obterCategorias();
 </script>
 
 <template>
