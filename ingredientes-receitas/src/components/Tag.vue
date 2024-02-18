c06c2287a293d91e6b08c8b1271d03a1ab98a9d3
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-18 08:48:22 -0300

    Add Tag component

---
+++ ingredientes-receitas/src/components/Tag.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  props: {
+    texto: {
+      type: String,
+      required: true
+    },
+  },
+};
+</script>
+
+<template>
+  <span>{{ texto }}</span>
+</template>
