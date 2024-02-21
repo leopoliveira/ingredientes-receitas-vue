f9fca5b24d2e48860607d71ba06bcbbf3a4e784e
Leonardo Oliveira <poliveira.leonardo@gmail.com>
2024-02-20 21:46:45 -0300

    Add IngredienteSelecionado component

---
+++ ingredientes-receitas/src/components/IngredienteSelecionado.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { ref } from "vue";
+import Tag from "./Tag.vue";
+
+export default {
+  props: {
+    ingrediente: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    Tag,
+  },
+  data() {
+    return {
+      selecionado: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <button
+    class="ingrediente"
+    v-on:click="selecionado = !selecionado"
+    :aria-pressed="selecionado">
+    <Tag
+      :texto="ingrediente"
+      :ativa="selecionado" />
+  </button>
+</template>
+
+<style scoped>
+.ingrediente {
+  cursor: pointer;
+}
+</style>
